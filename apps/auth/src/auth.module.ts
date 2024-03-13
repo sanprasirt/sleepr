d2744312d5e06d05a5c321e065d8b68383840dac
Sanprasirt boonma <sanprasirt@techex.co.th>
2024-03-13 14:04:50 +0700

    Update Configservice and users service

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersModule } from './users/users.module';
+import { LoggerModule } from 'nestjs-pino';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule, 
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATON: Joi.number().required(),
+      })
+    }),
+    JwtModule.registerAsync({
+    useFactory: (configService: ConfigService) => ({
+      secret: configService.get<string>('JWT_SECRET'),
+      signOptions: { expiresIn: `${configService.get('JWT_EXPIRATON')}s` },
+    }),
+    inject: [ConfigService],
+  })],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -4,6 +4,8 @@ import { ReservationsController } from './reservations.controller';
 import { DatabaseModule, LoggerModule } from '@app/common';
 import { ReservationsRepository } from './reservations.repository';
 import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [DatabaseModule, 
@@ -13,6 +15,12 @@ import { ReservationDocument, ReservationSchema } from './models/reservation.sch
       }
     ]),
     LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      })
+    }),
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
