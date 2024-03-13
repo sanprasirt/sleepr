16a7c33414e46a052254a2833bbf45ae60b2964b
Sanprasirt boonma <sanprasirt@techex.co.th>
2024-03-13 09:04:13 +0700

    Update nest auth app

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-  console.log('Starting ReservationsModule');
+  console.log('Starting ReservationsModule API...');
   const app = await NestFactory.create(ReservationsModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
